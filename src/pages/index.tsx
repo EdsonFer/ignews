5a61792b55acd878ae3cafe2ef6e06b86ef88572
EdsonFer <edlivelifehd1z@gmail.com>
2022-01-16 21:44:38 -0300

    home ignews

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,30 @@
 import Head from "next/head";
+import { SubscribeButton } from "../components/SubscribeButton";
+
+import styles from "./home.module.scss";
 
 export default function Home() {
    return (
       <>
          <Head>
-            <title>Início | ig.news</title>
+            <title>Home | ig.news</title>
          </Head>
-         <div>Hello world</div>
+         <main className={styles.contentContainer}>
+            <section className={styles.hero}>
+               <span>👏Hey, Welcome</span>
+               <h1>
+                  News about <br />
+                  the <span>React</span> world.
+               </h1>
+               <p>
+                  Get acess to all the publications <br />
+                  <span>for $9.90 month</span>
+               </p>
+               <SubscribeButton />
+            </section>
+
+            <img src="/images/avatar.svg" alt="Girl coding" />
+         </main>
       </>
    );
 }
